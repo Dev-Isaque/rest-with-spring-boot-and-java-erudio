0d95dbd6299bec7b1b2a595cf2f315604ba9c512
Dev-Isaque <devisaquesoares@gmail.com>
2026-04-14 16:54:54 -0300

    .

---
+++ src/main/java/br/com/erudio/MathController.java
@@ -0,0 +1,43 @@
+package br.com.erudio;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController /*-  */
+@RequestMapping("/math") /*  */
+public class MathController {
+
+    // http://localhost:8080/math/sum/1/2
+    @RequestMapping("/sum/{numberOne}/{numberTwo}")
+    public Double sum(
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo) throws Exception {
+
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+            throw new IllegalArgumentException();
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty())
+            throw new IllegalArgumentException();
+
+        String number = strNumber.replace(",", ".");
+
+        return Double.parseDouble(strNumber);
+    }
+
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null || strNumber.isEmpty())
+            return false;
+
+        String number = strNumber.replace(",", ".");
+
+        return number.matches("^[0-9]+([.][0-9]+)?$");
+    }
+
+    // http://localhost:8080/math/subtraction/1/2
+    // http://localhost:8080/math/division/1/2
+}
